6bc58d40dc3601ad334b4a21c07f82b967348e89
PaulB-H <p.bernardhall@gmail.com>
2021-07-21 18:07:22 -0400

    script: create countChars fn

--- script.js
+++ script.js
@@ -2,3 +2,7 @@
 
 const userStringTextarea = document.getElementById("user-string-textarea");
 const stringlength = document.getElementById("stringlength");
+
+const countChars = () => {
+  stringlength.innerText = userStringTextarea.value.length;
+};
